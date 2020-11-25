5e93a35d532f282cd0edec2887f27ac743416969
MDavid <mdavid10@gmail.com>
2020-11-25 23:56:32 +0200

    Adding more urls\views\templates

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,5 +8,14 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     url(r'^$', views.index, name='index'),
+
+    # Show all topics.
+    url(r'^topics/$', views.topics, name='topics'),
+
+    # Detail page for a single topic
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+
+    # Page for adding a new topic
+    url(r'^new_topic/$', views.new_topic, name='new_topic'),
 ]
 
